16bd7d3e7bad2a96961070a414774cc1d3fd731b
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-07-16 12:40:03 +0530

    changes

---
+++ src/main/java/com/example/eurekaclient/ClientController.java
@@ -0,0 +1,27 @@
+package com.example.eurekaclient;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @GetMapping("/")
+    public String invokeService(){
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("eureka-service", false);
+        String serviceBaseUrl = instanceInfo.getHomePageUrl();
+        return restTemplate.getForObject(serviceBaseUrl, String.class);
+    }
+}
